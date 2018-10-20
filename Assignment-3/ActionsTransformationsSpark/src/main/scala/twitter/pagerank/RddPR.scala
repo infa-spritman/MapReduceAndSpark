09115a783f44f2b8663ff4f3ac8145e08c876a62
kodefear <pritmani.s@husky.neu.edu>
2018-10-20 14:01:04 -0400

    PR: RDD Ordering Done

--- Assignment-3/ActionsTransformationsSpark/src/main/scala/twitter/pagerank/RddPR.scala
+++ Assignment-3/ActionsTransformationsSpark/src/main/scala/twitter/pagerank/RddPR.scala
@@ -9,7 +9,6 @@ import scala.collection.mutable
 object RddPR {
 
 
-
   def main(args: Array[String]) {
     val logger: org.apache.log4j.Logger = LogManager.getRootLogger
 
@@ -34,20 +33,18 @@ object RddPR {
     val k: Int = args(0).toInt
 
     var graph: Seq[(Int, Int)] = Seq.empty[(Int, Int)]
-    val volume: Double = k*k
-    val intialPR: Double = 1.0/volume
+    val volume: Double = k * k
+    val intialPR: Double = 1.0 / volume
 
     for {i <- 1 to k * k
          if i % k == 1 || k == 1} {
       for (j <- 0 to k - 2) {
         graph = graph :+ (i + j, i + j + 1)
-//        ranks = ranks :+ (i + j, intialPR)
       }
       graph = graph :+ (i + k - 1, 0)
-//      ranks = ranks :+ (i + k - 1, intialPR)
     }
 
-    val graphRDD = sc.parallelize(graph,2).partitionBy( new HashPartitioner(k))
+    val graphRDD = sc.parallelize(graph, 2).partitionBy(new HashPartitioner(k))
 
     // Some code here to make sure that graph has a Partitioner. This is needed for avoiding shuffling in the join below.
     // Tell Spark to try and keep this pair RDD around in memory for efficient re-use
@@ -55,31 +52,31 @@ object RddPR {
 
     // Create the initial PageRanks, using the page count |V|, which can be passed through the context.
     // Function mapValues ensures that the same Partitioner is used as for the graph RDD.
-    var ranks = graphRDD.mapValues( x => intialPR ).union(sc.parallelize(Seq((0,0.0))))
-
-//    println(ranks.count())
-//
-//    ranks.collect().foreach(println)
+    var ranks = graphRDD.mapValues(x => intialPR).union(sc.parallelize(Seq((0, 0.0))))
 
     // Function extractVertices returns each vertex id m in n’s adjacency list as (m, n’s PageRank / number of n’s outlinks).
 
-    for (iterationCount <- 1 to k*k +1 ) {
+    for (iterationCount <- 1 to k + 1) {
       // Use Accumulator instead to determine when last iteration is reached
-      var temp = graphRDD.join( ranks ).map(x => (x._2._1,x._2._2))
+      var temp = graphRDD.join(ranks).map(x => (x._2._1, x._2._2))
 
-      var temp2 = temp.reduceByKey(_+_)
+      var temp2 = temp.reduceByKey(_ + _)
 
       var delta = temp2.lookup(0)(0)
 
-      ranks = temp2.map(x => if(x._1 != 0) (x._1, delta/volume + x._2) else (x._1, 0.0))
+      var tempSeq = Seq.empty[(Int, Double)]
 
       for {i <- 1 to k * k
            if i % k == 1 || k == 1} {
-        ranks = ranks.union(sc.parallelize(Seq((i, delta/volume))))
+        tempSeq = tempSeq :+ (i, delta / volume)
       }
+
+      ranks = temp2.map(x => if (x._1 != 0) (x._1, delta / volume + x._2) else (x._1, 0.0)).union(sc.parallelize(tempSeq))
+
     }
 
-    logger.info("Count" + ranks.aggregate(0.0)((x,y) => x + y._2 , _+_))
+    logger.info("Count" + ranks.aggregate(0.0)((x, y) => x + y._2, _ + _))
+    ranks.top(k)(Ordering[(Double, Int)].on(x => (x._2,-x._1))).foreach(println)
 
   }
 }
